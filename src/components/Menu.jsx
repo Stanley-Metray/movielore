fb5d8f11ea08fefcaffa7d02b0998e12cbff433c
Stanley Metray <stanleymetray@gmail.com>
2023-07-19 21:18:46 +0530

    Fixed one bug in menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,6 @@
-import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const Menu = (props) => {
-
-    const [searchTitle, setSearchTitle] = useState("");
-
     const getCategory = (e) => {
         props.getCategory(e.target.getAttribute("data-bs-name"));
     }
