ce1b8e89455e0fdd05b7e27f1205077ab2d0b02b
Stanley Metray <stanleymetray@gmail.com>
2023-07-19 21:21:31 +0530

    Fixed one bug in menu

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,12 +1,9 @@
 import { Link } from "react-router-dom";
 
 const Menu = (props) => {
+
     const getCategory = (e) => {
         props.getCategory(e.target.getAttribute("data-bs-name"));
-    }
-
-    const getQuery = (e) => {
-        setSearchTitle(e.target.value);
     }
 
     // const submitHandler = (e) => {
